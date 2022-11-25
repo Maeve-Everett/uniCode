4992818e18a9b01d31ba411b6b237cadc4427a8f
Kieran Everett <kieraneverett03@gmail.com>
2022-11-25 03:07:13 +0000

    Update task2.js

--- ProgrammingFundamentals/task2.js
+++ ProgrammingFundamentals/task2.js
@@ -1,3 +1,4 @@
+/*
 // Creating global variables for calculating the delta time
 let timeAtLastFrame = new Date();
 let deltaTime;
@@ -18,4 +19,47 @@ while (true) {
     timeAtLastFrame = new Date();
     
     timeBasedMovement(movementSpeed);
+}
+*/
+
+
+
+
+function round(num) {
+    return Math.round(num * 100) / 100;
+}
+
+function lerp(start, end, progress) {
+    return start + (end - start) * progress;
+}
+
+function easeIn(time) {
+    return time * time;
+}
+
+function easeOut(time) {
+    return 1 - (easeIn(1 - time));
+}
+
+function easeInOut(time) {
+    return lerp(easeIn(time), easeOut(time), time);
+}
+
+function spike(time) {
+    if (time <= 0.5) {
+        return easeIn(time / 0.5);
+    } else {
+        return easeIn((1 - time) / 0.5);
+    }
+}
+
+function sine(time) {
+    return Math.sin(time * Math.PI);
+}
+
+for (i = 0; i <= 1; i += 0.1) {
+    //console.log(round(easeInOut(i)));
+    //console.log(easeOut(i));
+    //console.log(round(spike(i)));
+    console.log(sine(i));
 }
